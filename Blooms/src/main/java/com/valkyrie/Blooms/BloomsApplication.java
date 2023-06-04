0f8686f1258a72d5cb8b08432b66af52b8cade36
unknown <adi504097@gmail.com>
2023-06-04 13:48:15 +0700

    add dependencies

---
+++ Blooms/src/main/java/com/valkyrie/Blooms/BloomsApplication.java
@@ -0,0 +1,13 @@
+package com.valkyrie.Blooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomsApplication.class, args);
+	}
+
+}
---
+++ Blooms/src/test/java/com/valkyrie/Blooms/BloomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valkyrie.Blooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
