f59ec45cec02e72c8d27ee1528bb96317ea72d4c
unknown <adi504097@gmail.com>
2023-06-07 09:02:33 +0700

    add url connection

---
+++ Blooms/src/main/java/com/valkyrie/Blooms/makingHttp.java
@@ -0,0 +1,16 @@
+package com.valkyrie.Blooms;
+
+import java.util.Scanner;
+
+public class makingHttp {
+
+    Scanner in = new Scanner(System.in);
+
+    public static void main (String args[]){
+
+System.out.println("Masukan tahun : ");
+int tahun = in.nextInt();
+    
+}
+
+}
